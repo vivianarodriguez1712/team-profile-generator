dc2e72c48cc5b34463a77194aa4298fad373d87a
viviana rodriguez <vivianaprodriguez@live.com>
2022-08-17 21:34:42 -0400

    creating tests

---
+++ tests/Employee.test.js
@@ -0,0 +1,43 @@
+const Employee = require("../lib/Employee");
+
+test('not sure', () => {
+    const employee = new Employee();
+  
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+  
+  module.exports = Employee;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function(val1, val2) {
+//     if (val1 === val2) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
