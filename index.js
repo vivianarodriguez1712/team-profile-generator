478cab4b8e06abd43dbf9038b56afa4d4e7d701d
viviana rodriguez <vivianaprodriguez@live.com>
2022-08-18 11:43:31 -0400

    added questions

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
+
+const employeeQuestions [
+    {
+        type: 'input',
+        name: 'name'
+        message: 'Enter managers name'
+    }
+
+    {
+        type: 'input',
+        name: 'employee_id'
+        message: 'Enter employee ID'
+    }
+
+    {
+        type: 'input',
+        name: 'email'
+        message: 'Enter email address'
+    }
+
+    {
+        type: 'input',
+        name: 'office_number'
+        message: 'Enter office number'
+    }
+]
