905bf53cf84676b794272270914e13b94044e61b
viviana rodriguez <vivianaprodriguez@live.com>
2022-08-19 14:14:21 -0400

    finised index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
+const { finished } = require('stream');
 
-inquirer
-  .prompt([
+const employeeInfo = ([
     {
       type: 'input',
       name: 'name',
@@ -27,5 +27,64 @@ inquirer
       },
   ])
   .then(answers => {
-    console.info('Answer:', prompt);
+    console.info('Answer:', answers);
   });
+
+  const employeeQuestion = ([
+    {
+      type: 'list',
+      name: 'add',
+      message: 'Want to add an employee?',
+      choices: ['Engineer, Intern, Finish building my team']
+    }
+  ])
+
+  if (answers.add === "Engineer") {
+    const employeeAnswer = inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "github",
+          message: "Enter your GitHub user name?",
+        },
+    ])
+      const Engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        employeeAnswer.github
+      );
+
+      newemployeeInfo.push(Engineer);
+
+    } else if (answers.add === "Intern") {
+      const internInfo = inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "school",
+            message: "Enter your school name",
+          },
+        ])
+        
+        const Intern = new Intern(
+          answers.name,
+          answers.id,
+          answers.email,
+          internInfo.school
+        );
+        newemployeeInfo.push(newIntern);          
+    }
+    
+    const finish = ([
+      {
+        type: 'list',
+        name: 'done',
+        message: 'Are you done building you team?',
+        choice: ['yes, no'],
+      }
+  ])
+
+  .then(finished (answers) => {
+    console.info('Answer:', answers);
+  };
