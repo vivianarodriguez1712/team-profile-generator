9bad12904e605169b8515a41e3825fe04943a9c9
viviana rodriguez <vivianaprodriguez@live.com>
2022-08-20 13:00:12 -0400

    first and second prompt works, select choices work

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
-const { finish } = require('stream');
 
-const employeeInfo = () => {
-   return inquirer.prompt([
+const employeeInfo = async () => {
+   await inquirer.prompt([
      {
        type: 'input',
        name: 'name',
@@ -28,65 +27,83 @@ const employeeInfo = () => {
        },
  
    ]).then(answers => {
-     console.info('Answer:', answers);
-   })
- };
 
-  const employeeQuestion = ([
+  const employeeQuestion = async () => {
+    await inquirer.prompt([
     {
       type: 'list',
       name: 'add',
       message: 'Want to add an employee?',
-      choices: ['Engineer, Intern, Finish building my team']
+      choices: ['Engineer', 'Intern', 'Finish building my team']
     }
-  ])
 
-  if (answers.add === "Engineer") {
-    const employeeAnswer = inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "github",
-          message: "Enter your GitHub user name?",
-        },
-    ])
-      const Engineer = new Engineer(
-        answers.name,
-        answers.id,
-        answers.email,
-        employeeAnswer.github
-      );
+  ]).then(choice => {
+// this is for employeeQuestions
+    console.info('Answer:', choice);
+  })
+}
+employeeQuestion();
 
-      newemployeeInfo.push(Engineer);
+// for the first .then on line 29
+  //    console.info('Answer:', answers);
+   })
+  
+ };
 
-    } else if (answers.add === "Intern") {
-      const internInfo = inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "school",
-            message: "Enter your school name",
-          },
-        ])
+   employeeInfo();
+  
+
+   
+
+
+
+  // if (answers.add === "Engineer") {
+  //   const employeeAnswer = inquirer
+  //     .prompt([
+  //       {
+  //         type: "input",
+  //         name: "github",
+  //         message: "Enter your GitHub user name?",
+  //       },
+  //   ])
+//       const Engineer = new Engineer(
+//         answers.name,
+//         answers.id,
+//         answers.email,
+//         employeeAnswer.github
+//       );
+
+      // newemployeeInfo.push(Engineer);
+
+//     } else if (answers.add === "Intern") {
+//       const internInfo = inquirer
+//         .prompt([
+//           {
+//             type: "input",
+//             name: "school",
+//             message: "Enter your school name",
+//           },
+//         ])
         
-        const Intern = new Intern(
-          answers.name,
-          answers.id,
-          answers.email,
-          internInfo.school
-        );
-        newemployeeInfo.push(newIntern);          
-    }
+//         const Intern = new Intern(
+//           answers.name,
+//           answers.id,
+//           answers.email,
+//           internInfo.school
+//         );
+//         newemployeeInfo.push(newIntern);          
+//     }
     
- const finish = ([
-   {
-     type: 'list',
-     name: 'done',
-     message: 'Are you done building you team?',
-     choice: ['yes, no'],
-   }
-  ])
+//  const end = ([
+//    {
+//      type: 'list',
+//      name: 'done',
+//      message: 'Are you done building you team?',
+//      choice: ['yes, no'],
+//    }
 
-.then(finish, answers => {
-     console.info('Answer:', answers);
-    });
+// .then(end, answers => { 
+//      console.info('Answer:', answers);
+//     })
+
+//   ])
